a0dbf258ec65e9dd009f646771221d93cd697985
gurpreet29k <gurpreetk.0029@gmail.com>
2025-10-04 17:24:30 -0400

    Update Laptop_Mobile_Viewership.sql

--- Easy Questions/Laptop_Mobile_Viewership.sql
+++ Easy Questions/Laptop_Mobile_Viewership.sql
@@ -1,3 +1,5 @@
+--Problem- https://datalemur.com/questions/laptop-mobile-viewership
+
 SELECT 
   SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) laptop_views,
   SUM(CASE WHEN device_type in ('phone','tablet') THEN 1 ELSE 0 END) mobile_views
