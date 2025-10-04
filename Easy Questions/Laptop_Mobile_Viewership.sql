40232b525b1216cb7adf3ee2c08e1dea7282403d
gurpreet29k <gurpreetk.0029@gmail.com>
2025-10-04 17:14:45 -0400

    Update Laptop_Mobile_Viewership.sql

--- Easy Questions/Laptop_Mobile_Viewership.sql
+++ Easy Questions/Laptop_Mobile_Viewership.sql
@@ -1,4 +1,4 @@
 SELECT 
-SUM(CASE WHEN device_type = 'laptop' then 1 else 0 end) laptop_views,
-SUM(CASE WHEN device_type in ('phone','tablet') then 1 else 0 end) mobile_views
+  SUM(CASE WHEN device_type = 'laptop' then 1 else 0 end) laptop_views,
+  SUM(CASE WHEN device_type in ('phone','tablet') then 1 else 0 end) mobile_views
 FROM viewership
