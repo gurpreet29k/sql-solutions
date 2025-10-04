5f8c42bcc22174da6c7e8dad18b3f2f8c0629882
gurpreet29k <gurpreetk.0029@gmail.com>
2025-10-04 17:23:13 -0400

    Update Users_Third_Transaction.sql

--- Medium-level Questions/Users_Third_Transaction.sql
+++ Medium-level Questions/Users_Third_Transaction.sql
@@ -1,8 +1,9 @@
-Select 
-user_id, spend, transaction_date
-from (
-SELECT *,
-RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS rank
-FROM transactions
-) as ranked_transactions
-where rank = 3
+--Problem- https://datalemur.com/questions/sql-third-transaction
+
+SELECT user_id, spend, transaction_date
+FROM (
+  SELECT *,
+  RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS rank
+  FROM transactions
+  ) as ranked_transactions
+WHERE rank = 3
